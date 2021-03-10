0b7da71fcdd543828b19387c42e3c6c7738a5646
ArtoyTrumpeter <temathesuper@mail.ru>
2021-03-10 21:42:08 +0300

    change SuffTree files archivation

--- discrete analysis/lab5/pack.sh
+++ discrete analysis/lab5/pack.sh
@@ -2,6 +2,6 @@
 
 rm -f solution
 mkdir solution
-cp main.cpp SuffTree.{h,cpp} Makefile solution/
+cp main.cpp SuffTree.h SuffTree.cpp Makefile solution/
 tar -cvf solution.tar solution
 rm -r solution
